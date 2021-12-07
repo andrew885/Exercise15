5559246d0cf4146e662a6ca32cbed23716011793
Андрей Тарашкевич <andrew.tarashkevich9@gmail.com>
2021-12-07 23:46:42 +0300

    Add project files.

---
+++ ClassLibrary1/CustomException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class CustomException:System.Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ Exercise15/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using ClassLibrary1;
+using System.IO;
+using System.Collections.Generic;
+using Ionic.Zip;
+using System.Text;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string path = @"Q:\ItAcademy\Homework\Tasks";
+            string name = "Test.txt";
+            List<string> Folders = GetFolders(path);
+            Folders.Add(path);
+            List<string> Files = new List<string>();
+
+            foreach (var item in Folders)
+            {
+                string[] filePaths = Directory.GetFiles(@$"{item}", name);
+                foreach (var filepath in filePaths)
+                {
+                    Files.Add(filepath);
+                }
+            }            
+
+            if (Files.Count > 0)
+            {
+                using (ZipFile zip = new())
+                {
+                    foreach (var file in Files)
+                    {                       
+                        zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestSpeed;
+                        zip.AddFile(file);
+                        string zipfile = file.Remove(file.Length - 4);
+                        zip.Save(zipfile + ".zip");
+
+                        Console.WriteLine(zipfile + ".zip");
+                    }
+                }
+            }
+            else
+            {
+                throw new CustomException("Нет такого файла");
+            }
+
+        }
+
+        static List<string> Folders = new List<string>();
+        public static List<string> GetFolders(string path)
+        {            
+            DirectoryInfo dir = new DirectoryInfo(path);
+           
+            if (dir.GetDirectories().Length > 0)
+            {                
+                foreach (var item in dir.GetDirectories())
+                {
+                    Folders.Add(item.FullName);
+                    string childFolder = @$"{item.FullName}";
+                    GetFolders(@childFolder);
+                }                
+            }            
+            return Folders;
+        }        
+    }
+}
